fcae5f0bd7b8c356d32aa3d815049389e83d0b69
Asante-Adarkwa-Usman <asante.au96@gmail.com>
2023-07-04 15:18:22 -0400

    Variables and if statements

---
+++ main.dart
@@ -0,0 +1,31 @@
+//main file contains the main()
+
+//The main method
+void main() {
+  //IF STATEMENTS
+  //Simple If Statement
+  // int costOfItem = 50;
+  // if (costOfItem > 40) {
+  //   print('I cannot afford it');
+  // }
+
+  // //If Else Statement
+  // int costOfItem2 = 60;
+  // if (costOfItem2 > 50) {
+  //   print('I cannot afford it');
+  // } else {
+  //   print('I can afford it');
+  // }
+
+  //The Nested If Statement
+  double mark = 90;
+  if (mark >= 90) {
+    print('Excellent');
+  } else if (mark >= 79) {
+    print('Very Good');
+  } else if (mark >= 50) {
+    print('You failed');
+  } else if (mark < 0) {
+    print('Hey, enter a valid mark');
+  }
+}
---
+++ variables.dart
@@ -0,0 +1,37 @@
+//Single line comment
+/* 
+Multi line comment
+
+*/
+
+// VARIABLES
+//Declaring variables in dart
+
+String? name;
+//Same as String name = '';
+
+//Naming Conversions
+//When creating a variable, use Camel Casing
+//Example: firstName, lastName, fullName
+String firstNameOfStudent = 'Usman';
+int age = 20;
+double myGpa = 3.9000968698;
+bool isMarried = false;
+List<String>? user;
+//Lists are enclosed within
+List ages = [20, 50, 90, 70];
+double myTotalGpa = 0.3;
+
+String lastName = 'Asante';
+dynamic firstName = 'Usman';
+
+Map<dynamic, dynamic> userData = {
+  'fullName': 'Usman Asante',
+  'email': 'usman@example.com',
+  'age': 30,
+  'isMarried': false,
+  'height': 7.3
+}; //Maps are enclosed within curly braces
+
+dynamic names = []; // Use this type when you are not sure about the 
+//type of data you want to store in a variable
