0fa4fc1417d16771770a95040d4f214dadd2fe1c
Asante-Adarkwa-Usman <asante.au96@gmail.com>
2023-07-07 17:18:27 -0400

    switch case and dart operators

---
+++ if_statements.dart
@@ -0,0 +1,58 @@
+//IF STATEMENTS
+  //Simple If Statement
+  // int costOfItem = 50;
+  // if (costOfItem > 40) {
+  //   print('I cannot afford it');
+  // }
+
+  // //If Else Statement
+  // int costOfItem2 = 60;
+  // if (costOfItem2 > 50) {
+  //   print('I cannot afford it');
+  // } else {
+  //   print('I can afford it');
+  // }
+
+  //The Nested If Statement
+  // double mark = 90;
+  // if (mark >= 90) {
+  //   print('Excellent');
+  // } else if (mark >= 79) {
+  //   print('Very Good');
+  // } else if (mark >= 50) {
+  //   print('You failed');
+  // } else if (mark < 0) {
+  //   print('Hey, enter a valid mark');
+  // }
+
+  // Switch case
+
+  // int mark = 90;
+  // switch (mark) {
+  //   case 90:
+  //     print('Excellent');
+  //     break;
+  //   case 79:
+  //     print('Very Good');
+  //     break;
+  //   case 50:
+  //     print('You failed');
+  //     break;
+  //   default:
+  //     print('Hey, enter a valid mark');
+  //     break;
+  // }
+  // ;
+
+  // int age = 18;
+  // switch (age) {
+  //   case 18:
+  //     print('You are an adult');
+  //     break;
+  //   case 12:
+  //     print('You are a teenager');
+  //     break;
+  //   default:
+  //     print('You are a child');
+  //     break;
+  // }
--- main.dart
+++ main.dart
@@ -2,30 +2,53 @@
 
 //The main method
 void main() {
-  //IF STATEMENTS
-  //Simple If Statement
-  // int costOfItem = 50;
-  // if (costOfItem > 40) {
-  //   print('I cannot afford it');
-  // }
+  //DART OPERATORS
 
-  // //If Else Statement
-  // int costOfItem2 = 60;
-  // if (costOfItem2 > 50) {
-  //   print('I cannot afford it');
-  // } else {
-  //   print('I can afford it');
-  // }
+  //Arithmetic Operators
 
-  //The Nested If Statement
-  double mark = 90;
-  if (mark >= 90) {
-    print('Excellent');
-  } else if (mark >= 79) {
-    print('Very Good');
-  } else if (mark >= 50) {
-    print('You failed');
-  } else if (mark < 0) {
-    print('Hey, enter a valid mark');
-  }
+  int num1 = 10;
+  int num2 = 20;
+  // print(num1 + num2);
+  // print(num1 - num2);
+  // print(num1 * num2);
+  // print(num1 / num2);
+  // print(num1 % num2);
+  //print(num1 ~/ num2);
+  // print(num1++);
+  // num1 += 2;
+
+  // print(num1);
+  // print(num1--);
+  // print(++num1);
+  // print(--num1);
+  // List userData = [];
+  //  if(userData.length == 0) {
+  //     print('No data');
+  //   } else {
+  //     print('Data available');
+  //  }
+  //Relational Operators
+  print(num1 > num2); //T
+  print(num1 < num2); //T
+  print(num1 >= num2); //F
+  print(num1 <= num2); //T
+  print(num1 == num2); //F
+  print(num1 != num2); //T
+
+  //Logical operators
+  // print(num1 > num2 && num1 < num2);
+  // print(num1 > num2 || num1 < num2);
+  // print(!(num1 > num2 && num1 < num2));
+  // print(!(num1 > num2 || num1 < num2));
+  // print(!(num1 > num2 && num1 < num2) && (num1 > num2 || num1 < num2));
+  // print(!(num1 > num2 && num1 < num2) || (num1 > num2 || num1 < num2));
+  // print(!(num1 > num2 && num1 < num2) && (num1 > num2 || num1 < num2) || (num1 > num2 || num1 < num2));
+  // print(!(num1 > num2 && num1 < num2) && (num1 > num2 || num1 < num2) && (num1 > num2 || num1 < num2));
+  // print(!(num1 > num2 && num1 < num2) || (num1 > num2 || num1 < num2) || (num1 > num2 || num1 < num2));
+  // print(!(num1 > num2 && num1 < num2) && (num1 > num2 || num1 < num2) || (num1 > num2 || num1 < num2) || (num1 > num2 || num1 < num2));
+  // print(!(num1 > num2 && num1 < num2) && (num1 > num2 || num1 < num2) && (num1 > num2 || num1 < num2) || (num1 > num2 || num1 < num2));
+  // print(!(num1 > num2 && num1 < num2) && (num1 > num2 || num1 < num2) || (num1 > num2 || num1 < num2) || (num1 > num2 || num1 < num2) || (num1 > num2 || num1 < num2));
+  // print(!(num1 > num2 && num1 < num2) && (num1 > num2 || num1 < num2) && (num1 > num2 || num1 < num2) || (num1 > num2 || num1 < num2) || (num1 > num2 || num1 < num2));
+  // print(!(num1 > num2 && num1 < num2) && (num1 > num2 || num1 < num2) && (num1 > num2 || num1 < num2) && (num1 > num2 || num1 < num2) || (num1 > num2 || num1 < num2));
+  // print(!(num1 > num2 && num1 < num2) && (num1 > num2 || num1 < num2) && (num1 > num2 || num1 < num2) && (num1 > num2 || num1 < num2) || (num1 > num2 || num1 < num2) || (num1 > num2 || num1 < num2));
 }
